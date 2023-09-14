77828013095880255dc107410730141fc84938a7
Varun Nayyar <varun@neara.com>
2023-09-14 15:22:25 +1000

    Add a lot of coverage

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,11 +1,26 @@
 import numpy as np
+import pytest
 
 from spike import nn
 
 
+@pytest.fixture
+def data():
+	return np.random.random((100, 30))
 
-def test_dense_init():
+
+
+def test_dense_init(data):
 	layer = nn.Dense.xavier_init(30, 10)
-	data = np.random.random((100, 30))
 	out = layer.forward(data)
 	assert list(out.shape) == [100, 10]
+
+@pytest.mark.parametrize("module", [nn.Relu, nn.Softmax, lambda : nn.Dense.xavier_init(30, 10)])
+def test_mods(data, module):
+	layer = module()
+	out = layer.forward(data)
+
+def test_module(data):
+	mod = nn.Module([nn.Dense.xavier_init(30, 10), nn.Dense.xavier_init(10, 3)])
+	out = mod.forward(data)
+
