cf61d6376efcc648d3cfe3ae1bb255ebffa7803a
Varun Nayyar <varun@neara.com>
2023-09-14 14:46:18 +1000

    Add a test for activation

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -9,3 +9,10 @@ def test_dense_init():
 	data = np.random.random((100, 30))
 	out = layer.forward(data)
 	assert list(out.shape) == [100, 10]
+
+def test_activation():
+	activation = nn.Relu()
+	data = np.random.random((100, 30))
+	out = activation.forward(data)
+	assert list(out.shape) == (100, 30)
+	assert np.min(out) >= 0
